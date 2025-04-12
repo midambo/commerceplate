6b7b67ca172a5292d40ba4f22345e0c88e67539c
midambo <gregmidambo2@gmail.com>
2025-04-11 21:48:52 -0700

    Revert "..."
    This reverts commit 4220cc29827327cfbacbf05b9f1d0c6a78eff262.

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
   output: "standalone",
-  distDir: ".next",
   images: {
     remotePatterns: [
       {
